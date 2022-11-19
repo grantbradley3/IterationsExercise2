adcde933fa6b593062175b8ad4bd1d27dfb22f1c
Grant Bradley <grantbradleytiger13@gmail.com>
2022-11-18 20:51:16 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IterationStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>();
+
+            var myNumber = 0;
+
+
+            do
+            {
+                myNumber++;
+                numbers.Add(myNumber);
+
+            } while (myNumber<100);
+
+            while(myNumber <200)
+            {
+     
+                myNumber++;
+      
+                numbers.Add(myNumber);
+            }
+
+            Console.WriteLine("Increase:");
+
+
+            foreach (var item in numbers)
+            {
+                Console.WriteLine($"{item}");
+            }
+             
+            Console.WriteLine("");
+            Console.WriteLine("Decrease:");
+
+            for (int i = 199; i <= numbers.Count && i >= 0; i--)
+            
+            {
+                Console.WriteLine($"{numbers[i]}");
+            }
+        }
+    }
+}
